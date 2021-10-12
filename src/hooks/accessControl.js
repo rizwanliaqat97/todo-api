9a3d57017a6ba057fd4782520ef8da107bf623ff
Rizwan Liaqat <rizwanliaqat97@gmail.com>
2021-10-12 23:29:49 +0500

    chore: ✨ add access control on todos

---
+++ src/hooks/accessControl.js
@@ -0,0 +1,12 @@
+module.exports =
+  (options = {}) =>
+  async (context) => {
+    const { params } = context;
+    const { user, query } = params;
+    if (!user?._id) {
+      throw new Error("Permission denied");
+    }
+    const populated = { ...query, userId: user._id };
+    context.query = populated;
+    return context;
+  };
--- src/services/todos/todos.hooks.js
+++ src/services/todos/todos.hooks.js
@@ -1,34 +1,34 @@
-const { authenticate } = require('@feathersjs/authentication').hooks;
-const addUserId = require('../../hooks/userId.hook');
+const { authenticate } = require("@feathersjs/authentication").hooks;
+const addUserId = require("../../hooks/userId.hook");
 
 module.exports = {
-    before: {
-        all: [authenticate('jwt')],
-        find: [],
-        get: [],
-        create: [addUserId()],
-        update: [],
-        patch: [],
-        remove: []
-    },
+  before: {
+    all: [authenticate("jwt"), accessControl()],
+    find: [],
+    get: [],
+    create: [addUserId()],
+    update: [],
+    patch: [],
+    remove: [],
+  },
 
-    after: {
-        all: [],
-        find: [],
-        get: [],
-        create: [],
-        update: [],
-        patch: [],
-        remove: []
-    },
+  after: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: [],
+  },
 
-    error: {
-        all: [],
-        find: [],
-        get: [],
-        create: [],
-        update: [],
-        patch: [],
-        remove: []
-    }
+  error: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: [],
+  },
 };
